80f0b3dae9760d98d3b42f38026bc9169adedc4b
kirillmihalych <tiltlessman@gmail.com>
2024-08-11 22:42:39 +0300

    [3] structure the html for the board

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello world!(again)</h1>
+    <TrelloBoard />
   </div>
 </template>
---
+++ components/TrelloBoard.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import type { Column } from '@/types'
+import { nanoid } from 'nanoid'
+
+const colums = ref<Column[]>([
+  {
+    id: nanoid(),
+    title: 'Backlog',
+    tasks: [
+      {
+        id: nanoid(),
+        title: 'Create marketing landing page',
+        createdAt: new Date(),
+      },
+      {
+        id: nanoid(),
+        title: 'Develop cool new feature',
+        createdAt: new Date(),
+      },
+    ],
+  },
+  {
+    id: nanoid(),
+    title: 'In Progress',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Selected for Dev',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'QA',
+    tasks: [],
+  },
+  {
+    id: nanoid(),
+    title: 'Complete',
+    tasks: [],
+  },
+])
+</script>
+
+<template>
+  <div>
+    <div v-for="column in colums" :key="column.id">
+      <header>
+        {{ column.title }}
+      </header>
+      <p v-for="task in column.tasks" :key="task.id">
+        {{ task.title }}
+      </p>
+    </div>
+  </div>
+</template>
